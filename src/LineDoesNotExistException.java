34464b54d4f3a3f7d7ebf5cea95fb293b982771a
anshsaluja <157537413+anshsaluja@users.noreply.github.com>
2025-07-24 22:23:15 +0530

    Update LineDoesNotExistException.java

--- src/LineDoesNotExistException.java
+++ src/LineDoesNotExistException.java
@@ -3,12 +3,6 @@ package src;
 /**
  * An exception thrown in the <CODE>SecurityManager</CODE> class to indicate that 
  * lines entered to remove does not exist.
- * @author 
- * 		Ansh Saluja, SBU ID #: 115184358
- * <dt><b>Assignment:</b><dd>
- *    Homework #2 for CSE 214, Spring 2023
- * <dt><b>Date:</b><dd>
- *    March 31st, 2023
  */
 public class LineDoesNotExistException extends Exception {
 	/**
