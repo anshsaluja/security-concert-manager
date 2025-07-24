54e5fec9b92b2e80c2f5e2a41d14449b54abf1f4
anshsaluja <157537413+anshsaluja@users.noreply.github.com>
2025-07-24 22:22:36 +0530

    Update AllLinesEmptyException.java

--- src/AllLinesEmptyException.java
+++ src/AllLinesEmptyException.java
@@ -3,12 +3,6 @@ package src;
 /**
  * An exception thrown in the <CODE>SecurityManager</CODE> class to indicate that 
  * All Lines are empty and no line can be removed.
- * @author 
- * 		Ansh Saluja, SBU ID #: 115184358
- * <dt><b>Assignment:</b><dd>
- *    Homework #2 for CSE 214, Spring 2023
- * <dt><b>Date:</b><dd>
- *    March 31st, 2023
  */
 public class AllLinesEmptyException extends Exception {
 	/**
